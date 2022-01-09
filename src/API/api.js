e9043d13ba1288610f75b22f95a2c75e9efee660
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2022-01-09 14:58:46 +0200

    api+ markup

--- src/API/api.js
+++ src/API/api.js
@@ -4,9 +4,9 @@ axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.params = {  api_key: '7483e932537577abdaa29f08ebe6fdbf'};
 
 
-// /trending/get-trending список самых популярных фильмов на сегодня для создания коллекции на главной странице.
-// /search/search-movies поиск кинофильма по ключевому слову на странице фильмов.
-// /movies/get-movie-details запрос полной информации о фильме для страницы кинофильма.
+// + /trending/get-trending список самых популярных фильмов на сегодня для создания коллекции на главной странице.
+// + /search/search-movies поиск кинофильма по ключевому слову на странице фильмов.
+// + /movies/get-movie-details запрос полной информации о фильме для страницы кинофильма.
 // /movies/get-movie-credits запрос информации о актёрском составе для страницы кинофильма.
 // /movies/get-movie-reviews запрос обзоров для страницы кинофильма.
 
@@ -14,7 +14,40 @@ export const  Trending = async () => {
   try {
     const  response = await axios.get('/trending/all/day');
     return response.data;
-  } catch (event) {
-    console.error(event);
+  } catch (e) {
+    console.error(e);
   }
 };
+export const  Search = async (query,page) => {
+  try {
+    const  response = await axios.get(`/search/movie?&query=${query}&page=${page}`);
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+export const MovieById = async (movieId) => {
+  try {
+    const response = await axios.get(`/movie/${movieId}`);
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+export const Credits = async (movieId) => {
+  try {
+    const response = await axios.get(`/movie/${movieId}/credits`);
+    return response.data.cast;
+  } catch (e) {
+    console.error(e);
+  }
+};
+export const Reviews = async (movieId) => {
+  try {
+    const response = await axios.get(`/movie/${movieId}/reviews`);
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,12 @@
-import { Trending } from "../../API/api";
+import { Trending, Search, MovieById, Credits, Reviews } from "../../API/api";
+import { Fragment, useState, useEffect } from "react";
 
 export default function App() {
-  const apiData = async () => {
+  const [query, setQuery] = useState("car");
+  const [page, setPage] = useState(2);
+  const [movieId, setMovieId] = useState(8859);
+
+  const apiTrending = async () => {
     try {
       const data = await Trending();
       console.log(data);
@@ -9,7 +14,43 @@ export default function App() {
       console.error(e);
     }
   };
-  apiData();
+  //apiTrending();
+  const apiSearch = async () => {
+    try {
+      const data = await Search(query, page);
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  //apiSearch();
+  const apiMovieById = async () => {
+    try {
+      const data = await MovieById(movieId);
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  //apiMovieById();
+  const apiCredits = async () => {
+    try {
+      const data = await Credits(movieId);
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  //apiCredits();
+  const apiReviews = async () => {
+    try {
+      const data = await Reviews(movieId);
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  apiReviews();
   return (
     <div className="App">
       <p>init</p>
