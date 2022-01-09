e9102485cd57eede5d55db333b5a8c1b99658965
AleksandrKotenok <83491711+AleksandrKotenok@users.noreply.github.com>
2022-01-09 17:00:53 +0200

    fix dom version

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
+import { HomePage } from "../HomePage/HomePage";
+import { MoviesPage } from "../MoviesPage/MoviesPage";
 import { Trending, Search, MovieById, Credits, Reviews } from "../../API/api";
 import { Fragment, useState, useEffect } from "react";
-
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 export default function App() {
   const [query, setQuery] = useState("car");
   const [page, setPage] = useState(2);
@@ -52,8 +54,18 @@ export default function App() {
   };
   apiReviews();
   return (
-    <div className="App">
-      <p>init</p>
-    </div>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+      </ul>
+
+      <Route path="/" exact component={HomePage} />
+      <Route path="/movies" component={MoviesPage} />
+    </BrowserRouter>
   );
 }
---
+++ src/components/MoviesPage/MoviesPage.jsx
@@ -0,0 +1,3 @@
+export const MoviesPage = () => {
+  return <p>MOVIES</p>;
+};
